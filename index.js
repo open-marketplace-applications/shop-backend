40c872755b3dde7b640d67f252ee6b4e61e6d07c
huhn511 <sehe89@gmail.com>
2020-04-30 00:00:56 +0200

    add ipm starter

---
+++ index.js
@@ -0,0 +1,41 @@
+const paymentModule = require('iota-payment')
+const app = require('express')()
+
+app.get("/", (req, res) => {
+    res.send('hello world from shop backend!');
+});
+
+let options = {
+    api: true,
+    dashboard: true,
+    websockets: true
+    // ...
+}
+
+let server = paymentModule.createServer(app, options)
+
+// Start server with iota-payment dashboard on '/iotapay' and api on '/iotapay/api'
+server.listen(5000, () => {
+    console.log(`Server started on http://localhost:5000 `)
+    console.info(`Please visit http://localhost:5000/iotapay in your browser`)
+})
+
+//Create an event handler which is called, when a payment was created
+var onPaymentCreated = function (payment) {
+    console.log('payment created!', payment);
+}
+
+//Create an event handler which is called, when a payment was created
+var onPaymentIncoming = function (payment) {
+    console.log('payment incoming!', payment);
+}
+
+//Create an event handler which is called, when a payment was successfull
+var onPaymentSuccess = function (payment) {
+    console.log('payment success!', payment);
+}
+
+//Assign the event handler to the events:
+paymentModule.onEvent('paymentCreated', onPaymentCreated);
+paymentModule.onEvent('paymentIncoming', onPaymentIncoming);
+paymentModule.onEvent('paymentSuccess', onPaymentSuccess);
